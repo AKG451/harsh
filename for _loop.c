cdfae9f9330a9c3984c9a4101cc86fb33c2be301
Aryan Gupta <akg451154@gmail.com>
2025-10-01 15:06:44 +0530

    Implement for loop to print integers

---
+++ for _loop.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+    int a,b;
+    printf("Enter the integer till where to print:\n ");
+    scanf("%d",&a);
+    for(int i=1;i<=a;i++){
+        printf("%d\n",i);
+    }
+    
+    return 0;
+}
