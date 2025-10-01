eae240d9da47aa08786ac26e736e43945ca262d7
Aryan Gupta <akg451154@gmail.com>
2025-10-01 15:11:00 +0530

    Add while loop to print integers up to user input

---
+++ while_loop.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+    int a,b,i=1;
+    printf("Enter the integer till where to print:\n ");
+    scanf("%d",&a);
+while(i<=a){
+    printf("%d\n",i);
+    i++;
+}
+    
+    return 0;
+}
