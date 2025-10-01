b6cacb0e5fe4665d20a19ca1f731fe54d3572af7
Aryan Gupta <akg451154@gmail.com>
2025-10-01 14:50:29 +0530

    Add basic arithmetic operations in harsh1.c

---
+++ harsh1.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n;
+    int a=7,b=11;
+    printf("%d\n",a);
+    printf("%d\n",a+=b);
+    printf("%d\n",a*=b);
+    
+    return 0;
+}
